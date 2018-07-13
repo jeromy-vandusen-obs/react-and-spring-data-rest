18b8316fda8c4566b352508c7f353a6d5c888615
Jeromy Van Dusen <jvandusen@obsglobal.com>
2018-07-13 15:06:33 -0500

    Implement React front-end and integrate with back-end.

---
+++ src/main/java/com/obsglobal/boot/react/web/HomeController.java
@@ -0,0 +1,12 @@
+package com.obsglobal.boot.react.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
